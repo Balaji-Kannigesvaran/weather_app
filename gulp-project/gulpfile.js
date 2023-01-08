83439828c1fae263443afe95a1ce82793b3c8515
Prakash Ravichandran <prakashmtech1998@gmail.com>
2023-01-08 18:07:04 +0530

    added reload-browser sync

--- gulp-project/gulpfile.js
+++ gulp-project/gulpfile.js
@@ -8,17 +8,29 @@ const sass = require('gulp-sass')(require('sass'));
 function generateCSS(cb) {
     src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(dest('public/stylesheets'));
+        .pipe(dest('public/stylesheets'))
+        .pipe(sync.stream());
     cb();
 }
-
 exports.css = generateCSS;
 
 function watchFiles(cb) {
-  //  watch('views/**.ejs', generateHTML);
     watch('sass/**.scss', generateCSS);
-  //  watch([ '**/*.js', '!node_modules/**'], parallel(runLinter, runTests));
 }
 
 exports.watch = watchFiles;
+
+const sync = require("browser-sync").create();
+
+function browserSync(cb) {
+    sync.init({
+        server: {
+            baseDir: "./public"
+        }
+    });
+    watch('sass/**.scss', generateCSS);
+    watch("./public/**.html").on('change', sync.reload);
+}
+
+exports.sync = browserSync;
 
