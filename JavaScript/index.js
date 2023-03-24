d64a7221bebb2b938ca945332ca08af6aa477f2c
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2023-03-24 15:16:18 +0530

    refractor: removed reset css

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -6,10 +6,35 @@ const handleIncrement = () => {
   console.log(counter);
 };
 
-$(".toggle-btn").on("click", function () {
-  console.log("clicked the button");
-  $(".visibility").toggleClass(".hide");
-});
+// $(".toggle-btn").on("click", function () {
+//   console.log("clicked the button");
+//   $(".visibility").toggleClass(".hide");
+// });
+
+// mockapi call
+
+const AIR_KEY = "427f7ef80457a39a26407e17ef0d604339190901";
+const API_KEY = "20a36f8e1152244bbbd9ac296d3640f2";
+const lat = 44.34;
+const log = 10.99;
+var aqi = "";
+var sunrise = "";
+
+fetch(`https://api.waqi.info/feed/geo:${lat};${log}/?token=${AIR_KEY}`)
+  .then((res) => res.json())
+  .then((res) => {
+    aqi = res.data.aqi;
+    document.getElementById("aqi").innerText = aqi;
+
+    if (aqi >= 0 && aqi <= 50) {
+      document.querySelector("#air_quality_status").innerText = `Good`;
+    }
+
+    console.log("aqi is =" + aqi);
+    console.log(res);
+  });
+
+fetch  
 
 // Live Clock Js
 var clock = document.getElementById("clock");
